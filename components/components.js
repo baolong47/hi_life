df46c9100c784939bb199de198c8b2c936411f09
981564057@qq.com <981564057@qq.com>
2017-09-03 15:33:35 +0800

    新增广告模块和第二个轮播图

--- components/components.js
+++ components/components.js
@@ -30,13 +30,25 @@ var main ={
 }
 /* 首页模块 */
 var index = {
+	data(){
+		return {
+			swiper1:'./data/swiper1.json',
+			swiper2:'./data/swiper2.json'
+		}
+	},
 	template : '<section class="hi-life-index">'
 			  +'<header><div class="seat">深圳</div>'
 			  +'<div class="search-bar"><div class="search-bar-tool"><input type="text" placeholder="搜索" @click.prevent="toSearch"></div></div>'
 			  +'<div class="notice"><i class="notice-img"></i></div></header>'
 			  +'<section class="hi-life-index-content">'
-			  +'<swiper-view></swiper-view>'
+			  +'<swiper-view :swiperUrl="swiper1"></swiper-view>'
 			  +'<fenlei-view></fenlei-view>'
+			  +'<swiper-view :swiperUrl="swiper2"></swiper-view>'
+			  +'<section class="advertisement">'
+			  +'<div class="advertisement-item"><img src="./image/advertisement/guanggao1.png"></div>'
+			  +'<div class="advertisement-item"><img src="./image/advertisement/guanggao2.png"></div>'
+			  +'<div class="advertisement-item"><img src="./image/advertisement/guanggao3.png"></div></section>'
+			  +'<hr/>'
 			  +'</section></section>',
 	methods : {
 		toSearch : function(){
@@ -44,16 +56,21 @@ var index = {
 		}
 	}
 }
+/* 购物车 页面*/
 var shopCar = {
 	template : '<div>购物车</div>'
 }
+/* 会员中心页面 */
 var custom = {
 	template : '<div>会员中心</div>'
 }
+/* 轮播图 */
 var swiper = Vue.component('swiper-view',{
+	props :['swiperUrl'],
 	data(){
 		return {
-			swiperJson:null
+			swiperJson:null,
+			axjxUrl :this.swiperUrl
 		}
 	},
 	template : '<div class="swiper-view">'
@@ -62,7 +79,7 @@ var swiper = Vue.component('swiper-view',{
 			  +'</mt-swipe></div>',
 	created :function(){
 		var _this =this;
-		axios.get('./data/swiper1.json')
+		axios.get(this.swiperUrl)
 		.then(function(response){
 			_this.swiperJson = response.data;
 		})
@@ -82,7 +99,7 @@ var fenlei = Vue.component('fenlei-view',{
 			fenleiJson :null	
 		}
 	},
-	template : '<div class="fenlei"><div class="fenlei-item" v-for="(item,key) in fenleiJson"><img :src="item.icon"><p>{{item.label}}</p></div></div>',
+	template : '<div class="fenlei"><div class="fenlei-item" v-for="(item,key) in fenleiJson" @click="fenleiDetail"><img :src="item.icon"><p>{{item.label}}</p></div></div>',
 	created : function(){
 		var _this = this;
 		axios.get('./data/fenleiJson.json')
@@ -92,5 +109,10 @@ var fenlei = Vue.component('fenlei-view',{
 		.catch(function(){
 			_this.$toast("加载数据失败!");
 		});
+	},
+	methods : {
+		fenleiDetail : function(){
+			this.$toast("加载分类详情页");
+		}
 	}
 })
