2ca8544495006b68113a89353044b24dec24c549
lrs <lrs@192.168.1.107>
2017-09-03 10:17:03 +0800

    新增首页路由

--- components/components.js
+++ components/components.js
@@ -2,13 +2,39 @@
 var main ={
 	data(){
 		return {
-			
+			selected:'index',
+			tabJson:null
 		}
 	},
-	template : '<router-view></router-view>'
+	template : '<div><router-view></router-view>'
 			  +'<mt-tabbar v-model="selected">'
-				+'<mt-tab-item v-for="item in tab" @click.native="indexSwap(item.id)" :id="item.id">'
-					+'<img slot="icon" :src="item.imgUrl">{{item.label}}'
+				+'<mt-tab-item v-for="(item,key) in tabJson" @click.native="indexSwap(item.id)" :id="item.id">'
+					+'<img slot="icon" :src="item.icon">{{item.label}}'
 				+'</mt-tab-item>'
-			+'</mt-tabbar></div>'
+			+'</mt-tabbar></div>',
+	created : function(){
+		var _this =this;
+		axios.get('./data/tabBottom.json')
+		.then(function(response){
+			_this.tabJson = response.data;
+		})
+		.catch(function(){
+			this.$toast("加载失败!");
+		});
+	},
+	methods : {
+		indexSwap:function(val){
+			this.$router.push(val);
+		}
+	}
+}
+/* 首页模块 */
+var index = {
+	template : '<div>5657567</div>'
+}
+var shopCar = {
+	template : '<div>购物车</div>'
+}
+var custom = {
+	template : '<div>会员中心</div>'
 }
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var router = new VueRouter({ // 路由要挂载在vue实例上
 routes:[
 	{path:'/',component:main,
 	children:[
-		{path:'/',component:index,name:'index'},
+		{path:'index',component:index,name:'index'},
 		{path:'shopCar',component:shopCar,name:'shopCar'},
 		{path:'custom',component:custom,name:'custom'}
 	]}
